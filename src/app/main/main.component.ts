f3f8655e16691d6fd7f64fb86107847307fbae11
marcelabueeduar <marcela.cerda@bue.edu.ar>
2022-10-28 01:18:49 -0300

    primer version

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+  data={
+    "nombre":"",
+    "apellido":"",
+    "email":"",
+    "imagen":"",
+    "celular":"",
+    "ciudad":"",
+    "pais":"",
+    "experienciaLab":[
+      {"empresa":"",
+       "periodo":"",
+       "tareasRealizada":""
+       } 
+   ]
+  }
+  get = (): void =>{
+    let url:string = "../../assets/datos.json";
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+         this.data=data 
+         console.log(data)   
+         
+         
+      })
+  }
+
+  constructor() { 
+    this.get()
+  }
+
+  ngOnInit(): void {
+  }
+
+}
